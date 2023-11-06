4bf86bd1189681046319ace979ef898bbffa67e2
FelipeMessias2 <141263594+FelipeMessias2@users.noreply.github.com>
2023-11-06 10:55:22 -0300

    Create Cliente.java

---
+++ Cliente.java
@@ -0,0 +1,12 @@
+public class Cliente 
+{
+ private int cpf;  
+ private String nome;
+ private String email; 
+ public Cliente(int cpf, String nome, String email)    
+ {
+  this.cpf = cpf;
+  this.nome = nome;
+  this.email = email;
+ }
+}
