829f734f7d19f0e4d1a7856d0664dbc0194fc636
FelipeMessias2 <141263594+FelipeMessias2@users.noreply.github.com>
2023-11-06 10:56:12 -0300

    Create Aplicativo.java

---
+++ Aplicativo.java
@@ -0,0 +1,15 @@
+public class Aplicativo 
+{
+ private int codigo;
+ private String nome;
+ private String so;//Sistema Operacional
+ private double preço;//valor mensal da licença
+ public Aplicativo(int codigo, String nome, String so, double preço )
+ {
+  this.codigo = codigo;
+  this.nome = nome;
+  this.so =so;
+  this.preço = preço;
+ }    
+
+}
